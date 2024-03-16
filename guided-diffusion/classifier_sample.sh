40907b26a6d7b90a6b730cd78f600756c1d01fef
NeverFade <chuanlukk@163.com>
2024-03-17 00:11:33 +0800

    update adm

---
+++ guided-diffusion/classifier_sample.sh
@@ -0,0 +1,4 @@
+SAMPLE_FLAGS="--batch_size 4 --num_samples 20 --timestep_respacing 250"
+MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond True --diffusion_steps 1000 --image_size 256 --learn_sigma True --noise_schedule linear --num_channels 256 --num_head_channels 64 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
+LABEL_FLAGS="--labels 0 1 2 3"
+python classifier_sample.py $MODEL_FLAGS --classifier_scale 1.0 --classifier_path models/256x256_classifier.pt --model_path models/256x256_diffusion.pt $SAMPLE_FLAGS $LABEL_FLAGS
